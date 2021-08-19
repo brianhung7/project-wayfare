f238aa6a2e228695f8b59ce932db9b39e0e16a9c
Leo Cucinell <leocucinell@gmail.com>
2021-08-19 16:12:37 -0700

    FINISHED UPDATE PAGE

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,12 @@
 from django.urls import path
 from . import views
-from .views import Home, Signup, Profile, PostDetail, UpdateProfile
+from .views import Home, Signup, ProfileView, PostDetail, UpdateProfile
 
 
 urlpatterns = [
 	path('', Home.as_view(), name="home"),
     path('accounts/signup/', Signup.as_view(), name="signup"),
-    path('accounts/profile/', Profile.as_view(), name="profile"),
+    path('accounts/profile/', ProfileView.as_view(), name="profile_view"),
     path('posts/<int:pk>/', PostDetail.as_view(), name="post_detail"),
     path('accounts/profile/update', UpdateProfile.as_view(), name="update_profile"), 
 ]
