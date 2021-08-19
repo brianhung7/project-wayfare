46e790edacc4096be76936e934515db9bba79bf7
Leo Cucinell <leocucinell@gmail.com>
2021-08-19 15:13:37 -0700

    working on update view

--- main_app/forms.py
+++ main_app/forms.py
@@ -12,3 +12,11 @@ class ProfileUpdateForm(forms.ModelForm):
     class Meta:
         model = Profile
         fields = ['current_city', 'avatar']
+
+    #Saving issue in form_two.save(), overwriting to supply correct info
+    def save(self, user=None):
+        user_profile = super(ProfileUpdateForm, self).save(commit=False)
+        if user:
+            user_profile.user = user
+        user_profile.save()
+        return user_profile
