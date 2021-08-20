310173379586fdf0c296b99480110927f65a7915
brianhung7 <brian.texts@gmail.com>
2021-08-20 15:02:46 -0700

    seed file adjustments

--- db/seed.sql
+++ db/seed.sql
@@ -1,11 +1,13 @@
 INSERT INTO main_app_city(name,img) 
-    VALUES('Los Angeles','https://i.imgur.com/wasB1CM.jpeg');
+    VALUES('San Francisco','https://i.imgur.com/QEc3wRH.jpeg');
+INSERT INTO main_app_city(name,img) 
+    VALUES('Gibraltar','https://i.imgur.com/4FVxgyO.png');
+INSERT INTO main_app_city(name,img) 
+    VALUES('London','https://i.imgur.com/RWVJx9e.jpeg');
 INSERT INTO main_app_city(name,img) 
     VALUES('Paris','https://i.imgur.com/tv7ZdLZ.jpeg');
 INSERT INTO main_app_city(name,img) 
     VALUES('Hong Kong','https://i.imgur.com/mGmOD5S.jpeg');
-INSERT INTO main_app_city(name,img) 
-    VALUES('Brooklyn','https://i.imgur.com/ZayrnME.jpeg');
 INSERT INTO main_app_city(name,img) 
     VALUES('Dubai','https://i.imgur.com/Pm4IrNz.jpeg');
 INSERT INTO main_app_city(name,img) 
@@ -13,8 +15,14 @@ INSERT INTO main_app_city(name,img)
 
 
 INSERT INTO main_app_post(title, content, created_at, city_id, user_id) VALUES 
-    ('I love LA!','I had the most amazing time here!', '2020-05-07 18:57:42.517205-07' ,1, 1);
+    ('I love this place!','I had the most amazing time here!', '2020-05-07 18:57:42.517205-07' ,1, 1);
 INSERT INTO main_app_post(title, content, created_at, city_id, user_id) VALUES 
     ('The City of Love','I had the most amazing time here with my family!', '2021-05-09 18:57:42.517205-07' ,2, 1);
 INSERT INTO main_app_post(title, content, created_at, city_id, user_id) VALUES 
     ('The Best Trip!','I cant wait to go here again! Oh wow!', '2021-06-07 18:57:42.517205-07' ,3, 1);
+INSERT INTO main_app_post(title, content, created_at, city_id, user_id) VALUES 
+    ('I love this place!','I had the most amazing time here!', '2010-05-07 18:57:42.517205-07' ,4, 1);
+INSERT INTO main_app_post(title, content, created_at, city_id, user_id) VALUES 
+    ('The City of Love','I had the most amazing time here with my family!', '2021-01-02 18:57:42.517205-07' ,5, 1);
+INSERT INTO main_app_post(title, content, created_at, city_id, user_id) VALUES 
+    ('The Best Trip!','I cant wait to go here again! Oh wow!', '2019-03-07 18:57:42.517205-07' ,6, 1);
