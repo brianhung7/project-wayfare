e49c6a6531d5d3511ce28b53dbeb00fb1dbcb6a1
brianhung7 <brian.texts@gmail.com>
2021-08-22 12:07:25 -0700

    Min/max char limits for title/content on posts

--- main_app/models.py
+++ main_app/models.py
@@ -23,7 +23,7 @@ class Profile(models.Model):
 
 
 class Post(models.Model):
-    title = models.CharField(max_length=100)
+    title = models.CharField(max_length=200)
     content = models.TextField(max_length=5000)
     created_at = models.DateTimeField(auto_now_add=True)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="post")
